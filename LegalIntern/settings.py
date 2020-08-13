ff7275169bd9eaa58af5f12d936d1d7e54c3f1bd
Apoorv Garg <u.garg.10@gmail.com>
2020-08-13 19:55:13 +0530

    Security

--- LegalIntern/settings.py
+++ LegalIntern/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'gv@o7ak5%br01m_$ehin1-#32e(let%67mp@r9c!u1etd^7pnf'
+SECRET_KEY = 
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
